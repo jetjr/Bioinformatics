8e33346093ed39556366342b8143a354fcb1ef5e
jetjr <jamesthornton@email.arizona.edu>
2017-09-25 19:12:45 -0700

    fasta fragmenter

---
+++ bioinformatics/fragmenter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import math
+
+from Bio import SeqIO
+
+header = 1
+size = 150
+
+for seq_record in SeqIO.parse("virus_contigs.fasta", "fasta"):
+    seq_len = len(seq_record.seq)
+    num = range(seq_len // size)
+    print(seq_len)
+    begin = 0
+    end = 150
+    for i in num:
+        print(">",header)
+        print(str(seq_record.seq[begin:end]))
+        begin = begin + size
+        end = end + size
+        header = header + 1
+  
